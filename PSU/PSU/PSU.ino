ccae238ccc5996a84f6952506c882871fe46e564
jonas2707 <jons777@live.dk>
2021-08-26 14:59:35 +0200

    finished animation

--- PSU/PSU/PSU.ino
+++ PSU/PSU/PSU.ino
@@ -32,18 +32,20 @@ void setup() {
 void loop() {
   //  theaterChase(pixels.Color(Red, Green, Blue), Start, NumLeds, Interval, stepSize);
    int dir = (millis()/1000%2)*2-1;
+   int col1 = (millis()/1000%2);
+   int col2 = ((millis()/1000)+1)%2;
    
   // 230V AC
-  theaterChase(pixels.Color(15, 0, 0), 0, 34, 10, 5*dir);         // Top
-  theaterChase(pixels.Color(0, 0, 15), 423, 31, 10, 5*dir);       //Bottom
+  theaterChase(pixels.Color(20*col1, 0, 20*col2), 0, 34, 10, 5*dir);         // Top
+  theaterChase(pixels.Color(20*col2, 0, 20*col1), 423, 31, 10, 5*dir);       //Bottom
 
   // 12V AC
-  theaterChase(pixels.Color(15, 10, 0), 34, 123, 50, 3*dir);    // Top
-  theaterChase(pixels.Color(15, 0 , 15), 369, 54, 50, 3*dir);   // Bottom
+  theaterChase(pixels.Color(20*col1, 0, 20*col2), 34, 123, 25, 3*dir);    // Top
+  theaterChase(pixels.Color(20*col2, 0, 20*col1), 369, 54, 25, 3*dir);   // Bottom
   
   // 12V DC
-  theaterChase(pixels.Color(0, 10, 0), 262, 107, 50, -3);   // Top
-  theaterChase(pixels.Color(0, 10, 15), 157, 105, 50, -3);  // Bottom
+  theaterChase(pixels.Color(20, 0, 0), 262, 107, 25, -3);   // Top
+  theaterChase(pixels.Color(0, 0, 20), 157, 105, 25, -3);  // Bottom
   pixels.show(); // Update strip with new contents
   pixels.clear();
   // pixels.fill(pixels.Color(0, 0, 0), 0, 454);
